77ac0825eb712f1aee71834bfe0fc08399f492af
Aryya Gangopadhyay <gangopad@Aryyas-MacBook-Air.local>
2018-12-13 20:19:57 -0500

    disabling right click

--- js/success_scripts.js
+++ js/success_scripts.js
@@ -19,6 +19,31 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
+document.addEventListener('keydown', function() {
+  if (event.keyCode == 123) {
+    /* alert("This function has been disabled to prevent you from stealing my code!"); */
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    /* alert("This function has been disabled to prevent you from stealing my code!"); */
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    /* alert("This function has been disabled to prevent you from stealing my code!"); */
+    return false;
+  }
+}, false);
+
+if (document.addEventListener) {
+  document.addEventListener('contextmenu', function(e) {
+    /* alert("This function has been disabled to prevent you from stealing my code!"); */
+    e.preventDefault();
+  }, false);
+} else {
+  document.attachEvent('oncontextmenu', function() {
+    /* alert("This function has been disabled to prevent you from stealing my code!"); */
+    window.event.returnValue = false;
+  });
+}
+
 (function($) {
     "use strict"; 
 	
